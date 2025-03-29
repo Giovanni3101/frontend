10f879cef61b59cec6aeb202dc449f080dfbbf40
Giov3888 <giovannimahasano@gmail.com>
2025-03-29 23:30:41 +0200

    pt

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
@@ -15,5 +16,4 @@ export default defineConfig({
       },
     },
   },
-  base: '/'
 });
